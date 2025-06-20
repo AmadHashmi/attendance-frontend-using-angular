eed6800490e7672dc53f4d0fef22857eef235e79
Amad Ul Hassan <ah@indigilabs.com>
2025-06-20 23:29:06 +0200

    added validation and error messages

--- src/app/basic/login/login.component.ts
+++ src/app/basic/login/login.component.ts
@@ -15,7 +15,7 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: [null, [Validators.required]],
-      password: [null, [Validators.required]],
+      password: [null, [Validators.required, Validators.minLength(6)]],
     });
   }
 }
